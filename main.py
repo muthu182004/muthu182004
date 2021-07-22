5c83ba95975d9083b182ad29b1682d1e970fb586
muthu182004 <87769164+muthu182004@users.noreply.github.com>
2021-07-22 10:43:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+mark1 = 71
+mark2 = 99
+print(mark1 + mark2)
+
+# 10th calculator
+sub1 = int(input("enter the tamil mark"))
+sub2 = int(input("enter the english mark"))
+sub3 = int(input("enter the maths mark"))
+sub4 = int(input("enter the science mark"))
+sub5 = int(input("enter the social science mark"))
+print(sub1 + sub2 + sub3 + sub4 + sub5)
+print("you pass the tenth standard")
+
+# check
+name = input()
+print("my name is "+ name)
